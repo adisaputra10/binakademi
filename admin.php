4d09ef41b8cf3ebdc416c61a344936378640b6f9
Adi Saputra <adisaputra.id@gmail.com>
2020-10-18 13:14:09 +0700

    update index.html

--- admin.php
+++ admin.php
@@ -26,6 +26,10 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" <!-- Template CSS -->
     <link rel="stylesheet" href="assets/stisla-assets/css/style.css">
     <link rel="stylesheet" href="assets/stisla-assets/css/components.css">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+
 </head>
 
 <body>
