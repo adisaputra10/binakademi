e378ddbe7640f255c658b7fd5aa1879029e38531
Adi Saputra <adisaputra.id@gmail.com>
2020-10-21 07:46:02 +0700

    delete assets

--- adminold.php
+++ adminold.php
@@ -13,9 +13,11 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:500,600,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" <!-- Template CSS -->
-    <link rel="stylesheet" href="asset/stisla-assets/css/style.css">
-    <link rel="stylesheet" href="asset/stisla-assets/css/components.css">
- 
+    <link rel="stylesheet" href="assets/stisla-assets/css/style.css">
+    <link rel="stylesheet" href="assets/stisla-assets/css/components.css">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
 
 </head>
 
@@ -60,101 +62,46 @@
                     <ul class="sidebar-menu">
                         <li class="menu-header ">Dashboard</li>
                         <li class="nav-item dropdown active">
-                            <a href="#" class="nav-link"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
+                            <a href=" " class="nav-link"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
                         </li>
- 
-
-
-                        <li class="menu-header">Informasi</li>
+                        <li class="menu-header">Management Siswa</li>
                         <li class="nav-item dropdown">
                             <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-user"></i>
+                                <span>Siswa</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="nav-link" href="">Data Siswa</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-header">Management Guru</li>
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-chalkboard-teacher"></i>
+                                <span>Guru</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="nav-link" href="">Data Guru</a>
+                                </li>
+                                <li><a class="nav-link" href="">Tambah Data Guru</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="menu-header">Management Materi</li>
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-book"></i>
                                 <span>Materi</span></a>
                             <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="">Informasi</a></li>
-                                <li><a class="nav-link" href="">Materi Informasi</a></li>
+                                <li><a class="nav-link" href="">Data Materi</a>
+                                </li>
+                                <li><a class="nav-link" href="">Tambah Materi</a>
+                                </li>
                             </ul>
-                       
                         </li>
-
-
-                        <li class="menu-header">Materi HTML</li>
+                        <li class="menu-header">About Developer</li>
                         <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-user"></i>
-                                <span>Materi</span></a>
+                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-address-card"></i>
+                                <span>Developer</span></a>
                             <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="">Pengenalan HTML</a></li>
-                                <li><a class="nav-link" href="">Struktur HTML</a></li>
+                           
                             </ul>
                         </li>
-
-                        <li class="menu-header">Materi CSS</li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-user"></i>
-                                <span>Materi</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="">Pengenalan CSS</a></li>
-                                <li><a class="nav-link" href="">Struktur CSS</a></li>
-                            </ul>
-                        </li>
-
-
-                        <li class="menu-header">Materi Javascript</li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-user"></i>
-                                <span>Materi</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="">Pengenalan CSS</a></li>
-                                <li><a class="nav-link" href="">Pengenalan Lanjutan</a></li>
-                                <li><a class="nav-link" href="">Pengenalan DOM</a></li>
-                            </ul>
-                        </li>
-
-
-                        <li class="menu-header">Semua Materi</li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-user"></i>
-                                <span>Materi</span></a>
-                            <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="">Command Line</a></li>
-                                <li><a class="nav-link" href="">Git dan Github</a></li>
-                                <li><a class="nav-link" href="">ReactJS</a></li>
-                                <li><a class="nav-link" href="">Database</a></li>
-                                <li><a class="nav-link" href="">NodeJS</a></li>
-                                <li><a class="nav-link" href="">React Native</a></li>
-                                <li><a class="nav-link" href="">PHP</a></li>
-                                <li><a class="nav-link" href="">Golang</a></li>
-                                <li><a class="nav-link" href="">Golang - Web Apps</a></li>
-                                <li><a class="nav-link" href="">PHP-Laravel</a></li>
-                                <li><a class="nav-link" href="">Nodejs-Restfull</a></li>
-                                <li><a class="nav-link" href="">Deploy Nodejs ke AWS</a></li>
-                                <li><a class="nav-link" href="">Deploy Nodejs ke GCP</a></li>
-                            </ul>
-                        </li>
-                       
-
-                        <li class="menu-header">Materi Advance</li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-user"></i>
-                                <span>Materi Advance</span></a>
-                            <ul class="dropdown-menu">
-   
-                                <li><a class="nav-link" href="">React Native</a></li>
-                                <li><a class="nav-link" href="">Chatbot With  Dialogflow</a></li>
-                                <li><a class="nav-link" href="">ReactJS - Redux</a></li>
-                                <li><a class="nav-link" href="">ReactJS - Redux(Axios) </a></li>
-                                <li><a class="nav-link" href="">Networking</a></li>
-                                <li><a class="nav-link" href="">VueJS</a></li>
-                                <li><a class="nav-link" href="">Flutter</a></li>
-                                <li><a class="nav-link" href="">AngularJS</a></li>
-                                <li><a class="nav-link" href="">PHP - Codeigniter</a></li>
-                                <li><a class="nav-link" href="">Ruby Basic</a></li>
-                                <li><a class="nav-link" href="">Docker</a></li>
-                                <li><a class="nav-link" href="">Java Basic</a></li>
-                                <li><a class="nav-link" href="">Django</a></li>
-                            </ul>
-                        </li>
-                       
-                     
                 </aside>
             </div>
             <!-- End Sidebar -->
@@ -166,120 +113,86 @@
                         <h1 style="font-size: 27px; letter-spacing:-0.5px; color:black;">Dashboard</h1>
                     </div>
                     <div class="row">
-
-
-
-                       
-                       
                         <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                             <div class="card card-statistic-1">
-                            <a href=""> 
-                                <div class="card-icon bg-success">
-                                    <i class="fas fa-chalkboard-teacher"></i>
+                                <div class="card-icon bg-primary">
+                                    <i class="far fa-user"></i>
                                 </div>
-                           
                                 <div class="card-wrap">
                                     <div class="card-header">
-                                        <h4>Apa Itu HTML</h4>
+                                        <h4>Siswa</h4>
                                     </div>
                                     <div class="card-body">
-                                        1
+                                  
                                     </div>
                                 </div>
-                                </a>
-
                             </div>
                         </div>
-
                         <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                             <div class="card card-statistic-1">
-                            <a href=""> 
-                                <div class="card-icon bg-success">
+                                <div class="card-icon bg-danger">
                                     <i class="fas fa-chalkboard-teacher"></i>
                                 </div>
-                           
                                 <div class="card-wrap">
                                     <div class="card-header">
-                                        <h4>Apa Itu HTML</h4>
+                                        <h4>Guru</h4>
                                     </div>
                                     <div class="card-body">
-                                        2
+                                      
                                     </div>
                                 </div>
-                                </a>
-
                             </div>
                         </div>
-
                         <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                             <div class="card card-statistic-1">
-                            <a href=""> 
-                                <div class="card-icon bg-success">
-                                    <i class="fas fa-chalkboard-teacher"></i>
+                                <div class="card-icon bg-warning">
+                                    <i class="fas fa-book"></i>
                                 </div>
-                           
                                 <div class="card-wrap">
                                     <div class="card-header">
-                                        <h4>Apa Itu HTML</h4>
+                                        <h4>Materi</h4>
+                                    </div>
+                                    <div class="card-body">
+                                    
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                            <div class="card card-statistic-1">
+                                <div class="card-icon bg-success">
+                                    <i class="fas fa-users"></i>
+                                </div>
+                                <div class="card-wrap">
+                                    <div class="card-header">
+                                        <h4>Kelas</h4>
                                     </div>
                                     <div class="card-body">
                                         3
                                     </div>
                                 </div>
-                                </a>
-
                             </div>
                         </div>
-
-                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-                            <div class="card card-statistic-1">
-                            <a href=""> 
-                                <div class="card-icon bg-success">
-                                    <i class="fas fa-chalkboard-teacher"></i>
-                                </div>
-                           
-                                <div class="card-wrap">
-                                    <div class="card-header">
-                                        <h4>Apa Itu HTML</h4>
-                                    </div>
-                                    <div class="card-body">
-                                        4
-                                    </div>
-                                </div>
-                                </a>
-
-                            </div>
-                        </div>
-
-                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-                            <div class="card card-statistic-1">
-                            <a href=""> 
-                                <div class="card-icon bg-success">
-                                    <i class="fas fa-chalkboard-teacher"></i>
-                                </div>
-                           
-                                <div class="card-wrap">
-                                    <div class="card-header">
-                                        <h4>Apa Itu HTML</h4>
-                                    </div>
-                                    <div class="card-body">
-                                        5
-                                    </div>
-                                </div>
-                                </a>
-
-                            </div>
-                        </div>
-
-
-
                     </div>
-
-               
+                    <div class="">
+                        <div class="card" style="width:100%;">
+                            <div class="card-body">
+                                <h2 class="card-title" style="color: black;">Tambah Materi?</h2>
+                                <hr>
+                                <p class="card-text">After I ran into Helen at a restaurant, I realized she was just office pretty drop-dead date put in in a deck for our standup today. Who's responsible for the ask for this request? who's responsible for the ask for this request? but moving the goalposts gain traction.</p>
+                                <a href=" " class="btn btn-success">Tambah Materi ⭢</a>
+                            </div>
+                        </div>
+                    </div>
                     <div class="">
                         <div class="hero text-white hero-bg-image" data-background="assets/stisla-assets/img/unsplash/eberhard-grossgasteiger-1207565-unsplash.jpg">
                             <div class=" hero-inner">
-                         
+                                <h1>Selamat Datang,  </h1>
+                                <p class="lead">After I ran into Helen at a restaurant, I realized she was just office pretty drop-dead date put in in a deck for our standup today. Who's responsible for the ask for this request? but moving the goalposts gain traction.</p>
+                                <div class="mt-4">
+                                    <a href=" " class="btn btn-outline-white btn-lg btn-icon icon-left"><i class="far fa-user"></i>
+                                        Data Siswa</a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -289,7 +202,7 @@
             <!-- Start Footer -->
             <footer class="main-footer">
                 <div class="text-center">
-                    Copyright &copy; 2020 <div class="bullet"></div> 
+                    Copyright &copy; 2020 <div class="bullet"></div><a href="https://github.com/syauqi">Syauqi Zaidan Khairan Khalaf</a>
                 </div>
             </footer>
             <!-- End Footer -->
@@ -306,10 +219,10 @@
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-    <script src="asset/stisla-assets/js/stisla.js"></script>
+    <script src="assets/stisla-assets/js/stisla.js"></script>
     <!-- Template JS File -->
-    <script src="asset/stisla-assets/js/scripts.js"></script>
-    <script src="asset/stisla-assets/js/custom.js"></script>
+    <script src="assets/stisla-assets/js/scripts.js"></script>
+    <script src="assets/stisla-assets/js/custom.js"></script>
 </body>
 
 </html>
